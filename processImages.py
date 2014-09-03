e5c85ff72f7fffcd22573c4f39d65e0b1f3f89e4
selim88 <slim.s1988@hotmail.fr>
2014-09-03 16:09:44 -0400

    Update processImages.py

--- processImages.py
+++ processImages.py
@@ -3,16 +3,19 @@ import concurrent.futures
 From app import BusbudBanner
 From app import images
 
+#function parallel_image_processing() gets images from images() and process them with different offered functions
+
 def parallel_image_processing :
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         future = executor.submit(images)
-        future1 = executor.submit( BusbudBanner.load, future.result())
-        future2 = executor.submit( BusbudBanner.blur, future.result(),"6")
-        future3 = executor.submit( BusbudBanner.scale_x, future.result(),"1500","BICUBIC")
-        future4 = executor.submit( BusbudBanner.crop_vertical, future.result())
-        future5 = executor.submit( BusbudBanner.crop_top, future.result(),"300")
-        future6 = executor.submit( BusbudBanner.crop_bottom, future.result(),"300")
-        future7 = executor.submit( BusbudBanner.crop_vmiddle, future.result(),"300")
-        future8 = executor.submit( BusbudBanner.save, future.result())
+        for fileobject in future.result() :
+            future1 = executor.submit( BusbudBanner.load, fileobject)
+            future2 = executor.submit( BusbudBanner.blur, fileobject,"6")
+            future3 = executor.submit( BusbudBanner.scale_x, fileobject,"1500","BICUBIC")
+            future4 = executor.submit( BusbudBanner.crop_vertical, fileobject)
+            future5 = executor.submit( BusbudBanner.crop_top, fileobject,"300")
+            future6 = executor.submit( BusbudBanner.crop_bottom, fileobject,"300")
+            future7 = executor.submit( BusbudBanner.crop_vmiddle, fileobject,"300")
+            future8 = executor.submit( BusbudBanner.save, fileobject)
       
       
